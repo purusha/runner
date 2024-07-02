3e3ab9ed576538245366ba77de77e616a3737cd9
alan toro <alanprog@gmail.com>
2024-07-02 16:54:20 -0400

    added XXX

--- src/main/java/org/acme/getting/started/commandmode/Req.java
+++ src/main/java/org/acme/getting/started/commandmode/Req.java
@@ -6,6 +6,7 @@ import lombok.Data;
 @Data
 public class Req {
 		
+	//XXX formato accettabile: content-type=application/json, vary=Host, vary=origin
 	private String headers;
 	
 	//XXX obbligatorio se method == POST or PUT
@@ -18,8 +19,10 @@ public class Req {
 	@NotBlank
 	private String url;
 	
+	//XXX formato accettabile: content-type=application/json, vary=Host, vary=origin
 	private String expectedHeaders;
 	
+	//se json ... deve essere confrontato come JsonNode
 	private String expectedBody;
 	
 	//XXX valori accettabili: https://en.wikipedia.org/wiki/List_of_HTTP_status_codes
